645c663712cbbaca0ba125774c52635952248d09
in83ane <ronnakorn.real@email.com>
2025-08-05 20:55:14 +0700

    refactor: change title JFrame

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("My Swing App");
+        JFrame frame = new JFrame("My CGI project");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 600);
 
